0f26dcc928719b8a38f9d8f5084433e05671fc8d
satthubongdem <49702341+satthubongdem@users.noreply.github.com>
2020-08-27 09:41:43 +0900

    復習

--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -12,9 +12,18 @@ namespace v0827
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            Point mp = MousePosition;
         }
     }
 }
